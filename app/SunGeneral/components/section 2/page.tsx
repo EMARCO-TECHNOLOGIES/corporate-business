109745bec3143de12a616050fcbc142696c8aeed
ADIL-SALAH <122431444+ADIL-SALAH@users.noreply.github.com>
2023-12-21 09:18:35 +0530

    varticles img responsive issue resolved and other updations

--- app/SunGeneral/components/section 2/page.tsx
+++ app/SunGeneral/components/section 2/page.tsx
@@ -19,14 +19,14 @@ function AboutUs() {
     We have every reason to be very satisfied with the results that have been achieved in the short span of 3 years Our strategic advice, coupled with prudent risk management, has helped our customers to perform to their revenue budget and beyond.`
 
     return (
-        <div className=' flex justify-center'>
-            <div className='flex md:flex-row flex-col lg:p-20 md:p-10 lg:w-[100%] md:mt-0 mt-10 items-center'>
-                <div className='md:w-1/3'>
-                    <img src="/home/SunGeneral.jpg" alt="" className='lg:w-[418px] lg:h-[437px] md:w-[280px] w-[180px] rounded-tl-full rounded-b-full' />
+        <div className=' flex justify-center' id='about'>
+            <div className='flex lg:flex-row flex-col lg:p-20 md:p-10 lg:w-[100%] lg:h-[600px] mt-10 items-center'>
+                <div className='lg:w-1/3'>
+                    <img src="/home/SunGeneral.jpg" alt="" className='lg:w-[418px] lg:h-[437px] md:w-[420px] w-[250px] lg:rounded-tl-full lg:rounded-b-full lg:rounded-tr-none rounded-br-full rounded-t-full' />
                 </div>
-                <div className='md:w-2/3 space-y-5 p-10'>
+                <div className='lg:w-2/3 space-y-5 p-10'>
                     <h1 className='text-4xl font-bold '>About Us</h1>
-                    <p className='text-lg'>
+                    <p className='lg:text-lg'>
                         {content}
                     </p>
                     <button className='border-2 border-green-600 rounded-2xl px-4 py-2 text-green-800 text-sm'>
--- app/SunOil/components/section 2/page.tsx
+++ app/SunOil/components/section 2/page.tsx
@@ -5,14 +5,14 @@ function AboutUs() {
     const content = 'Sunoil Trading Refined Oil Product Abroad LLC (SUN) is an energy company. It carries out the exploration, development and production of crude oil and natural gas, transportation of crude oil and production of liquefied petroleum gas (LPG). The company’s business operations are mainly confined in Arab and African countries. It also has participating interest in exploration blocks in Kampala and in various projects in Libya, Gabon, Nigeria, the US, Venezuela, Mozambique, Bangladesh, Russia, and Israel. The company also provides upstream related services and operates crude and product pipelines. SUN is headquartered in Dubai, United Arab Emirates.'
 
     return (
-        <div className=' flex justify-center'>
-            <div className='flex md:flex-row flex-col lg:p-20 md:p-10 lg:w-[100%] md:mt-0 mt-10 items-center'>
-                <div className='md:w-1/3'>
-                    <img src="/home/IMG_5.jpg" alt="" className='lg:w-[418px] lg:h-[437px] md:w-[280px] w-[180px] rounded-tl-full rounded-b-full' />
+        <div className=' flex justify-center' id='about'>
+            <div className='flex lg:flex-row flex-col lg:p-20 md:p-10 lg:w-[100%] lg:h-[600px] mt-10 items-center'>
+                <div className='lg:w-1/3'>
+                    <img src="/home/IMG_5.jpg" alt="" className='lg:w-[418px] lg:h-[437px] md:w-[420px] w-[250px] lg:rounded-tl-full lg:rounded-b-full lg:rounded-tr-none rounded-br-full rounded-t-full' />
                 </div>
-                <div className='md:w-2/3 space-y-5 p-10'>
+                <div className='lg:w-2/3 space-y-5 p-10'>
                     <h1 className='text-4xl font-bold '>About Us</h1>
-                    <p className='text-lg'>
+                    <p className='lg:text-lg'>
                         {content}
                     </p>
                     <button className='border-2 border-green-600 rounded-2xl px-4 py-2 text-green-800 text-sm'>
